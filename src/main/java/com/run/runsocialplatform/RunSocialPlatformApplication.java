424765e86c2ad3b011ab3ff61e12221ee9b14c93
王庚润 <3206201719@qq.com>
2026-01-04 09:36:38 +0800

    项目初始化

---
+++ src/main/java/com/run/runsocialplatform/RunSocialPlatformApplication.java
@@ -0,0 +1,11 @@
+package com.run.runsocialplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunSocialPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunSocialPlatformApplication.class, args);
+    }
+}
